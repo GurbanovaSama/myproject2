3075f474eb358c312601457ed6746622ab15f560
GurbanovaSama <samapg-ab205@code.edu.az>
2024-10-20 23:09:59 +0400

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace Executable
+{
+    public class Person
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public byte Age { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public Person (string username, string password, string name, string surname, byte age, DateTime birthday)
+        {
+            UserName = username;
+            Password = password;
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Birthday = birthday;
+        }
+        public Person()
+        {
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CustomValidator;
+using Validator = CustomValidator.Validator;
+
+
+
+namespace Executable
+
+
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Validator validator = new CustomValidator.Validator();
+            Person newPerson = new Person();
+
+
+            Console.WriteLine("Enter the username ");
+            newPerson.UserName = Console.ReadLine();
+            Console.WriteLine("Enter the password ");
+            newPerson.Password = Console.ReadLine();
+            Console.WriteLine("Enter the name ");
+            newPerson.Name = Console.ReadLine();
+            Console.WriteLine("Enter the surname ");
+            newPerson.Surname = Console.ReadLine();
+            Console.WriteLine("Enter the age ");
+            newPerson.Age = byte.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the birthday ");
+            newPerson.Birthday = DateTime.Parse(Console.ReadLine());    
+
+
+            if (validator.ValidateUserName(newPerson.UserName) &&
+                validator.ValidatePassword(newPerson.Password) &&
+                validator.ValidateName(newPerson.Name) &&
+                validator.ValidateSurname(newPerson.Surname) &&
+                validator.ValidateAge(newPerson.Age) &&
+                validator.ValidateBirthday(newPerson.Birthday))
+            {
+                Console.WriteLine("Every information is correct");
+            }
+            else
+            {
+                Console.WriteLine("The information is incorrect");
+            }
+
+
+
+        }
+    }
+}
